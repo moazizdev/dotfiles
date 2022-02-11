21268d09367b9dc27abec417e3a76a8fc1d2691e
mohamed-zezo <net_66@yahoo.com>
2022-02-11 21:33:01 +0200

    add random emojis to greeting message fish shel

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,20 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    alias dotfiles 'git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+	# Commands to run in interactive sessions can go here
+	
+	if string match -qir '.*\.utf-?8' -- $LANG $LC_CTYPE
+	    set -l animal_marine 🐳 🐋 🐬 🦭  🐟 🐠 🐡 🦈 🐙 🐚
+	    set -l animal_reptile 🐸 🐢
+	    set -l food_marine 🦀 🦞 🦐 🦑
+
+	    set fishes $animal_marine $animal_reptile $food_marine
+
+	    function fishes_greeting
+	      echo (random choice $fishes; random choice $fishes; random choice $fishes)
+	    end
+
+	    set fish_greeting "Welcome! Let's fish! "(fishes_greeting)
+	end
+	
+	#set -g fish_greeting "Welcome! Let's fish! 🐟 🐠"
+	alias dotfiles 'git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 end
